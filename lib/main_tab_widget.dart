c97c5ae85afd12daf5454a92248a1a572fea49df
SheinH <chocolateshein.htike@gmail.com>
2024-02-01 19:11:58 -0500

    Templates

---
+++ lib/main_tab_widget.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainTabWidget extends StatefulWidget {
+  const MainTabWidget({super.key});
+
+  @override
+  State<MainTabWidget> createState() => _MainTabWidgetState();
+}
+
+class _MainTabWidgetState extends State<MainTabWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text("Tabs go here lol"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.add),
+            label: "Check In",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.list_bullet),
+            label: "Entries",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.graph_square),
+            label: "Data",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
