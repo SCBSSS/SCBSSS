12fc8f21320f4abb0869348f8cd1fc4cddf73e7f
SheinH <chocolateshein.htike@gmail.com>
2024-02-06 12:52:48 -0500

    Starting code for timeline view

--- lib/main_tab_widget.dart
+++ lib/main_tab_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:scbsss/models/journal_entry.dart';
 import 'package:scbsss/tabs/add_entry_tab.dart';
 import 'package:scbsss/tabs/data_tab.dart';
 import 'package:scbsss/tabs/entries_tab.dart';
@@ -13,15 +14,49 @@ class MainTabWidget extends StatefulWidget {
 }
 
 class _MainTabWidgetState extends State<MainTabWidget> {
-  final tabs = [
-    AddEntryTab(),
-    EntriesTab(),
-    DataTab(),
-    SettingsTab(),
+
+  final dummyJournalEntries = [
+    JournalEntry(
+        id: 1,
+        mood: 1,
+        title: "Day 1 of classes",
+        entry: "Classes went well",
+        date: DateTime.now()),
+    JournalEntry(
+        id: 2,
+        mood: 3,
+        title: "Day 2 of classes",
+        entry: "Classes were okay",
+        date: DateTime.now()),
+    JournalEntry(
+        id: 3,
+        mood: 5,
+        title: "Day 3 of classes",
+        entry: "Classes were excellent",
+        date: DateTime.now()),
+    JournalEntry(
+        id: 4,
+        mood: 2,
+        title: "Day 4 of classes",
+        entry: "Classes were not so good",
+        date: DateTime.now()),
+    JournalEntry(
+        id: 5,
+        mood: 4,
+        title: "Day 5 of classes",
+        entry: "Classes were good",
+        date: DateTime.now()),
   ];
   int currentTabIndex = 0;
+
   @override
   Widget build(BuildContext context) {
+    final tabs = [
+      AddEntryTab(),
+      EntriesTab(dummyJournalEntries),
+      DataTab(),
+      SettingsTab(),
+    ];
     return Scaffold(
       body: tabs[currentTabIndex],
       bottomNavigationBar: BottomNavigationBar(
--- lib/tabs/entries_tab.dart
+++ lib/tabs/entries_tab.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:scbsss/models/journal_entry.dart';
+import 'package:scbsss/views/journal_entry_view.dart';
 
 class EntriesTab extends StatelessWidget {
+  List<JournalEntry> dummyJournalEntries;
+  EntriesTab(this.dummyJournalEntries);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Entries'),
+    return Center(
+      child: SafeArea(
+        child: Column(
+          children: dummyJournalEntries.map((e) => JournalEntryView(e)).toList(),
+        ),
+      ),
     );
   }
 }
