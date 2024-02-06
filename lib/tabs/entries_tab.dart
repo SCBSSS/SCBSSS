ce491ceadb87124e195e437c0a4b47cb74b1d590
SheinH <chocolateshein.htike@gmail.com>
2024-02-06 12:57:29 -0500

    EntriesTab is now stateful.

--- lib/tabs/entries_tab.dart
+++ lib/tabs/entries_tab.dart
@@ -2,16 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:scbsss/models/journal_entry.dart';
 import 'package:scbsss/views/journal_entry_view.dart';
 
-class EntriesTab extends StatelessWidget {
+class EntriesTab extends StatefulWidget {
   List<JournalEntry> dummyJournalEntries;
   EntriesTab(this.dummyJournalEntries);
+
+  @override
+  State<EntriesTab> createState() => _EntriesTabState();
+}
+
+enum EntriesTabViews {
+  timeline,
+calendar
+}
+class _EntriesTabState extends State<EntriesTab> {
+  var currentView = EntriesTabViews.timeline;
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: SafeArea(
         child: Column(
-          children: dummyJournalEntries.map((e) => JournalEntryView(e)).toList(),
+          children: widget.dummyJournalEntries.map((e) => JournalEntryView(e)).toList(),
         ),
       ),
     );
