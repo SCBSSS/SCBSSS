2d59bf5d7e20fbdea9a5f8d2aa7552754fea2dab
Sam Phillips <armyf35@gmail.com>
2024-02-07 17:45:34 -0500

    Modified journal_entry
    - Changed date to nullable
    - Added fromJson
    - Added toMap

--- lib/models/journal_entry.dart
+++ lib/models/journal_entry.dart
@@ -3,7 +3,7 @@ class JournalEntry {
   final int mood; // Mood rating from 1 to 5
   final String title; // Optional title for the journal entry
   final String entry; // Optional journal text
-  final DateTime date; // Timestamp for when the journal entry was created
+  final DateTime? date; // Timestamp for when the journal entry was created
 
   JournalEntry({
     required this.id,
@@ -12,4 +12,24 @@ class JournalEntry {
     required this.entry,
     required this.date,
   });
+
+  factory JournalEntry.fromJson(Map<String, dynamic> map) {
+    return JournalEntry(
+      id: map['id'],
+      mood: map['mood'],
+      title: map['title'],
+      entry: map['entry'],
+      date: DateTime.tryParse(map['date'])
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'mood': mood,
+      'title': title,
+      'entry': entry,
+      'date': date
+    };
+  }
 }
