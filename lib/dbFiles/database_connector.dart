449f519754ce3236bb699e2dcaa19b53322c64c4
Sam Phillips <armyf35@gmail.com>
2024-02-14 02:06:48 -0500

    Modified database_connector - Replaced createTables method with migrationPlan

--- lib/dbFiles/database_connector.dart
+++ lib/dbFiles/database_connector.dart
@@ -1,74 +1,88 @@
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart' as sql;
 import 'package:path_provider/path_provider.dart';
+import 'package:sqflite_migration_plan/migration/sql.dart';
+import 'package:sqflite_migration_plan/sqflite_migration_plan.dart';
 
 class DatabaseHelper {
-  static Future<void> createTables(sql.Database db) async {
-    await db.execute(
-        """CREATE TABLE users (
-           id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-           username VARCHAR(64) NOT NULL UNIQUE,
-           email VARCHAR(100) NOT NULL UNIQUE,
-           hashed_password NOT NULL,
-           join_date DATE DEFAULT CURRENT_DATE,
-           preferences VARCHAR(100)
-         );"""
-    );
-    await db.execute(
-        """CREATE TABLE settings (
-           settings_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-           user_id INTEGER,
-           notification_preferences VARCHAR(100),
-           app_theme VARCHAR(100),
-           data_backup_options VARCHAR(100),
-           FOREIGN KEY(user_id) REFERENCES users(id)
-         );"""
-    );
-    await db.execute(
-        """CREATE TABLE user_activity (
-           activity_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-           user_id INTEGER,
-           type VARCHAR(20),
-           duration INTEGER,
-           timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-           additional_details VARCHAR(500),
-           FOREIGN KEY(user_id) REFERENCES users(id)
-         );"""
-    );
-    await db.execute(
-        """CREATE TABLE mood_entries (
-           entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-           user_id INTEGER,
-           timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-           mood_rating INTEGER NOT NULL,
-           notes VARCHAR(500),
-           related_activities INTEGER,
-           transcript_audio VARCHAR(500),
-           transcript_txt VARCHAR(1000),
-           transcript_summary VARCHAR(1000),
-           FOREIGN KEY(user_id) REFERENCES users(id)
-         );"""
-    );
-    await db.execute(
-        """CREATE TABLE meditation (
-           meditation_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-           title VARCHAR(50),
-           description VARCHAR(500),
-           duration INTEGER NOT NULL,
-           category VARCHAR(20) NOT NULL,
-           audio_file_url VARCHAR(500)
-         );"""
-    );
-  }
+  static MigrationPlan migrationPlan = MigrationPlan({
+      1: [
+        SqlMigration('''
+          CREATE TABLE users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            username VARCHAR(64) NOT NULL UNIQUE,
+            email VARCHAR(100) NOT NULL UNIQUE,
+            hashed_password NOT NULL,
+            join_date DATE DEFAULT CURRENT_DATE,
+            preferences VARCHAR(100)
+          );
+
+          CREATE TABLE settings (
+            settings_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id INTEGER,
+            notification_preferences VARCHAR(100),
+            app_theme VARCHAR(100),
+            data_backup_options VARCHAR(100),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+          );
+
+          CREATE TABLE user_activity (
+            activity_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id INTEGER,
+            type VARCHAR(20),
+            duration INTEGER,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            additional_details VARCHAR(500),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+          );
+
+          CREATE TABLE mood_entries (
+            entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            user_id INTEGER,
+            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            mood_rating INTEGER NOT NULL,
+            notes VARCHAR(500),
+            related_activities INTEGER,
+            transcript_audio VARCHAR(500),
+            transcript_txt VARCHAR(1000),
+            transcript_summary VARCHAR(1000),
+            FOREIGN KEY(user_id) REFERENCES users(id)
+          );
+
+          CREATE TABLE meditation (
+            meditation_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            title VARCHAR(50),
+            description VARCHAR(500),
+            duration INTEGER NOT NULL,
+            category VARCHAR(20) NOT NULL,
+            audio_file_url VARCHAR(500)
+          );
+          ''',
+          reverseSql: '''
+            DROP TABLE meditation;
+            DROP TABLE mood_entries;
+            DROP TABLE user_activity;
+            DROP TABLE settings;
+            DROP TABLE users;
+          '''
+        )
+      ]
+    });
 
   static Future<sql.Database> getDatabase() async {
     final directory = await getApplicationDocumentsDirectory();
     final dbPath = join(directory.path, 'scbsss_db.db');
+
     return sql.openDatabase(
       dbPath,
       onCreate: (db, version) {
-        // called if db doesn't exist and is being created
-        createTables(db);
+        return migrationPlan(db, version);
+      },
+      onUpgrade: (db, oldVersion, newVersion) {
+        return migrationPlan(db, oldVersion, newVersion);
+      },
+      onDowngrade: (db, oldVersion, newVersion) {
+        return migrationPlan(db, oldVersion, newVersion);
       },
       version: 1, // we will increment this whenever we make different schemas
     );
