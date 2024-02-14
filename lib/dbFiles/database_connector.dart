1256648b4a9f58bbb13ac58315faa0636ece75e6
SheinH <chocolateshein.htike@gmail.com>
2024-02-14 12:40:27 -0500

    Made changes to db schema to reflect models/JournalEntry.dart.

--- lib/dbFiles/database_connector.dart
+++ lib/dbFiles/database_connector.dart
@@ -6,68 +6,63 @@ import 'package:sqflite_migration_plan/sqflite_migration_plan.dart';
 
 class DatabaseHelper {
   static MigrationPlan migrationPlan = MigrationPlan({
-      1: [
-        SqlMigration('''
-          CREATE TABLE users (
-            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            username VARCHAR(64) NOT NULL UNIQUE,
-            email VARCHAR(100) NOT NULL UNIQUE,
-            hashed_password NOT NULL,
-            join_date DATE DEFAULT CURRENT_DATE,
-            preferences VARCHAR(100)
-          );
+    1: [
+      SqlMigration(
+        '''
+      CREATE TABLE settings (
+          settings_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          user_id INTEGER,
+          notification_preferences VARCHAR(100),
+          app_theme VARCHAR(100),
+          data_backup_options VARCHAR(100),
+          FOREIGN KEY(user_id) REFERENCES users(id)
+      );
 
-          CREATE TABLE settings (
-            settings_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            user_id INTEGER,
-            notification_preferences VARCHAR(100),
-            app_theme VARCHAR(100),
-            data_backup_options VARCHAR(100),
-            FOREIGN KEY(user_id) REFERENCES users(id)
-          );
+      CREATE TABLE user_activity (
+          activity_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          user_id INTEGER,
+          type VARCHAR(20),
+          duration INTEGER,
+          timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          additional_details VARCHAR(500),
+          FOREIGN KEY(user_id) REFERENCES users(id)
+      );
 
-          CREATE TABLE user_activity (
-            activity_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            user_id INTEGER,
-            type VARCHAR(20),
-            duration INTEGER,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            additional_details VARCHAR(500),
-            FOREIGN KEY(user_id) REFERENCES users(id)
-          );
+      CREATE TABLE mood_entries (
+          id INTEGER PRIMARY KEY,
+          mood INTEGER NOT NULL,
+          title TEXT,
+          entry TEXT,
+          date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+          FOREIGN KEY(recording_id) REFERENCES recordings(id) 
+      );
 
-          CREATE TABLE mood_entries (
-            entry_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            user_id INTEGER,
-            timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            mood_rating INTEGER NOT NULL,
-            notes VARCHAR(500),
-            related_activities INTEGER,
-            transcript_audio VARCHAR(500),
-            transcript_txt VARCHAR(1000),
-            transcript_summary VARCHAR(1000),
-            FOREIGN KEY(user_id) REFERENCES users(id)
-          );
+      CREATE TABLE recordings (
+          id INTEGER PRIMARY KEY,
+          audio_file_path TEXT NOT NULL,
+          duration REAL NOT NULL,
+          transcription TEXT
+      );
 
-          CREATE TABLE meditation (
-            meditation_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-            title VARCHAR(50),
-            description VARCHAR(500),
-            duration INTEGER NOT NULL,
-            category VARCHAR(20) NOT NULL,
-            audio_file_url VARCHAR(500)
-          );
-          ''',
-          reverseSql: '''
+      CREATE TABLE meditation (
+          meditation_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+          title VARCHAR(50),
+          description VARCHAR(500),
+          duration INTEGER NOT NULL,
+          category VARCHAR(20) NOT NULL,
+          audio_file_url VARCHAR(500)
+      );''',
+        reverseSql: '''
             DROP TABLE meditation;
             DROP TABLE mood_entries;
+            DROP TABLE recordings;
             DROP TABLE user_activity;
             DROP TABLE settings;
             DROP TABLE users;
-          '''
-        )
-      ]
-    });
+          ''',
+      )
+    ]
+  });
 
   static Future<sql.Database> getDatabase() async {
     final directory = await getApplicationDocumentsDirectory();
