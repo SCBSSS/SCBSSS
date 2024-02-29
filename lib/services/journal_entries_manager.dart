6f28cb360981f7cc6a99fa8cc5cd33b0bceec569
Sam Phillips <armyf35@gmail.com>
2024-02-29 16:15:29 -0500

    Renamed mood to journal from rebase

--- lib/services/journal_entries_manager.dart
+++ lib/services/journal_entries_manager.dart
@@ -7,13 +7,13 @@ class JournalManager {
   final ValueNotifier<List<JournalEntry>> _journalEntries = ValueNotifier<List<JournalEntry>>([]);
 
   Future<void> init() async {
-    _journalEntries.value = await _databaseService.getMoodEntry();
+    _journalEntries.value = await _databaseService.getJournalEntry();
   }
 
   ValueNotifier<List<JournalEntry>> get journalEntries => _journalEntries;
 
   Future<void> addEntry(JournalEntry entry) async {
     _journalEntries.value = List.from(_journalEntries.value)..add(entry);
-    _databaseService.insertMoodEntry(entry);
+    _databaseService.insertJournalEntry(entry);
   }
 }
