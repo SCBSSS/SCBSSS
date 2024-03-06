5c555043c424e5b975c98cf8091639ed2efffd78
CS9490 <cristianstatescu.cs9490@gmail.com>
2024-03-06 02:45:26 -0500

    added in another route to backend in order to make generated titles for people that don't want to make their own titles for entries (finished TODO marked in flutter entry tab)

--- flask_backend/app.py
+++ flask_backend/app.py
@@ -9,7 +9,11 @@ client = OpenAI(api_key=config['OPENAI_API_KEY'])
 
 app = Flask(__name__)
 
-# assumes you set the OpenAI API key in an environment variable called OPENAI_API_KEY in the .zshrc file
+
+## example curl to test: 
+#curl -X POST http://127.0.0.1:5000/summarize-entry \  
+#-H "Content-Type: application/json" \
+#-d '{"journal_entry": "today was a really weird day, but I hardly slept. I was pretty sad because of it. my dog is doing better today. I had my favorite cereal and it really motivated me. Specifically frosted flakes."}'
 
 @app.route('/summarize-entry', methods=['POST'])
 def summarize_entry():
@@ -34,6 +38,36 @@ def summarize_entry():
         return jsonify({'summary': summary})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/create_title', methods=['POST'])
+def create_title():
+    data = request.json
+    journal_entry = data.get('journal_entry')
+    if not journal_entry:
+        return jsonify({'error': 'No journal entry provided'}), 400
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {
+                    "role": "system",
+                    "content": "Create a title for the following content:"
+                },
+                {
+                    "role": "user",
+                    "content": journal_entry
+                }
+            ]
+        )
+        title = response.choices[0].message.content
+        return jsonify({'title': title})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+## in order to test this out, you can run the following cURL command in your terminal:
+#curl -X POST http://127.0.0.1:5000/create_title \
+#-H "Content-Type: application/json" \
+#-d '{"journal_entry": "today was a really weird day, but I hardly slept. I was pretty sad because of it. my dog is doing better today. I had my favorite cereal and it really motivated me. Specifically frosted flakes."}'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
