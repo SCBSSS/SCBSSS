632b2e7424fa7908dfcfada9fc73eac47a4134fc
CS9490 <cristianstatescu.cs9490@gmail.com>
2024-03-18 01:34:51 -0400

    changed around some of the UI to make the heatmap page look better and added in some details to the settings page

--- lib/tabs/settings_tab.dart
+++ lib/tabs/settings_tab.dart
@@ -1,10 +1,90 @@
 import 'package:flutter/material.dart';
 
 class SettingsTab extends StatelessWidget {
+  const SettingsTab({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Settings'),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        backgroundColor: Colors.teal[500],
+        appBar: AppBar(
+          backgroundColor: Colors.tealAccent[400],
+          title: const Text('Settings'),
+          bottom: const TabBar(
+            tabs: [
+              Tab(text: 'General'),
+              Tab(text: 'Account'),
+              Tab(text: 'Notifications'),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            GeneralSettings(),
+            Center(child: Text('Account Settings')),
+            Center(child: Text('Notification Settings')),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class GeneralSettings extends StatefulWidget {
+  const GeneralSettings({super.key});
+
+  @override
+  _GeneralSettingsState createState() => _GeneralSettingsState();
+}
+
+class _GeneralSettingsState extends State<GeneralSettings> {
+  bool _lightTheme = false;
+  bool _notificationsEnabled = false;
+  String _selectedLanguage = 'English';
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        SwitchListTile(
+          title: const Text('Light Theme'),
+          value: _lightTheme,
+          onChanged: (bool value) {
+            setState(() {
+              _lightTheme = value;
+            });
+          },
+        ),
+        SwitchListTile(
+          title: const Text('Enable Notifications'),
+          value: _notificationsEnabled,
+          onChanged: (bool value) {
+            setState(() {
+              _notificationsEnabled = value;
+            });
+          },
+        ),
+        ListTile(
+          title: const Text('Language'),
+          trailing: DropdownButton<String>(
+            value: _selectedLanguage,
+            onChanged: (String? newValue) {
+              setState(() {
+                _selectedLanguage = newValue!;
+              });
+            },
+            items: <String>['English', 'Spanish', 'French', 'German']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+        ),
+      ],
     );
   }
 }
