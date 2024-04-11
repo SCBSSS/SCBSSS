c0630fc995c30a97b78f8ae2cc88cb37cc70cda0
Bismahhasan <hasanbismah@gmail.com>
2024-04-10 21:10:32 -0400

    Scrollable calendar

--- lib/tabs/settings_tab.dart
+++ lib/tabs/settings_tab.dart
@@ -9,26 +9,27 @@ class SettingsTab extends StatelessWidget {
     return DefaultTabController(
       length: 3,
       child: Scaffold(
-        backgroundColor: Colors.white,
         appBar: AppBar(
-          title: const Text('Settings',
+          title: const Text(
+            'Settings',
             style: TextStyle(
               fontFamily: 'Poppins',
               fontWeight: FontWeight.bold,
               fontSize: 25,
+              color: Colors.white,
             ),
           ),
-          backgroundColor: Colors.orange[300],
+          backgroundColor: Colors.black,
           elevation: 0,
           centerTitle: true,
           bottom: const TabBar(
-            indicatorColor: Colors.blue,
-            labelColor: Colors.blue,
+            indicatorColor: Color.fromARGB(255, 144, 199, 168),
+            labelColor: Color.fromARGB(255, 144, 199, 168),
+            unselectedLabelColor: Colors.white,
             isScrollable: false,
             labelPadding: EdgeInsets.all(0),
             labelStyle: TextStyle(
-              fontWeight: FontWeight.w800,
-              fontSize: 16),
+                fontWeight: FontWeight.w800, fontSize: 16, color: Colors.white),
             tabs: [
               Tab(text: 'General'),
               Tab(text: 'Account'),
@@ -36,14 +37,24 @@ class SettingsTab extends StatelessWidget {
             ],
           ),
         ),
-        body: const TabBarView(
-          children: [
-            GeneralSettings(),
-            Center(child: Text(
-              'Account Settings',
-            selectionColor: Colors.lightBlueAccent)),
-            Center(child: Text('Notification Settings')),
-          ],
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                Color.fromARGB(255, 207, 233, 222)!,
+                Color.fromARGB(255, 205, 235, 237)!,
+              ],
+            ),
+          ),
+          child: const TabBarView(
+            children: [
+              GeneralSettings(),
+              Center(child: Text('Account Settings')),
+              Center(child: Text('Notification Settings')),
+            ],
+          ),
         ),
       ),
     );
@@ -102,7 +113,21 @@ class _GeneralSettingsState extends State<GeneralSettings> {
             }).toList(),
           ),
         ),
+        ListTile(
+          title: const Text('Data Backup'),
+          trailing: IconButton(
+            icon: const Icon(Icons.backup),
+            onPressed: () {
+              // Backup data
+            },
+          ),
+        ),
       ],
     );
   }
 }
+
+//colors saved for bg gradient
+// Color.fromARGB(255, 219, 234, 220)!,
+// Color.fromARGB(255, 94, 137, 134)!,
+// Color.fromARGB(255, 99, 126, 100)!,],
