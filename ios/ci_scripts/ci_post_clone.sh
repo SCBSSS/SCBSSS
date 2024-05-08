44d19eba7aa3cce9e82fee51301d02d4148e3c28
Sam Phillips <armyf35@gmail.com>
2024-05-08 16:40:43 -0400

    Modified xcode build script - Added empty .env file

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,6 +6,8 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
+touch .env
+
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 export PATH="$PATH:$HOME/flutter/bin"
