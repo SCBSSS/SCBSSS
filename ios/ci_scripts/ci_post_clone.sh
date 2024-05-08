d0fb175da6b0c18a6fdb4c569e9a4cbf37ba1345
Sam Phillips <armyf35@gmail.com>
2024-05-08 18:54:32 -0400

    Modified xcode cloud script - Possible fix for empty .env

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,7 +6,7 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-touch .env
+echo '' > .env
 
 # Install Flutter using git.
 git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
